91edbf3db85b26ba03691fde5143fa8185c85755
Adzal <adam.lockwood@orange.fr>
2022-06-15 11:22:09 +0200

    Change serializer Type

--- src/Menu.java
+++ src/Menu.java
@@ -1,68 +1,72 @@
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 import Format.*;
 
 import Utils.*;
 
-
 public class Menu {
-    public ArrayList<Support> supports;
+	public ArrayList<Support> supports;
 
-    public void displayMenu() {
+	public void displayMenu() {
 
-        try {
-            SaveNLoad snl = new SaveNLoad();
-            supports = snl.load();
-        } catch (Exception e) {
-            System.out.println("Une erreur est survenue");
-        }
+		try {
+			SaveNLoad snl = new SaveNLoad();
+			supports = snl.load();
 
-        // Question pour le choix de l'objet a créer
-        String question = "Que voulez créer ?\n1- Livre\n2- DVD\n3- CD\n0- STOP";
-        Integer userChoice = Scan.scanInteger(question);
-        Support s = null;
-        // tant que la réponse n'est pas 0 (STOP) on continue la création d'objet
-        while (userChoice != 0) {
+		} catch (Exception e) {
+			System.out.println("Une erreur est survenue");
+		}
 
-            switch (userChoice) {
-                case 1:
-                    s = new Livre(); // choix 1 crée un livre
-                    break;
-                case 2:
-                    s = new Dvd(); // choix 2 crée un Dvd
-                    break;
-                case 3:
-                    s = new Cd(); // choix 3 crée un Cd
-                    break;
-                // case 4:
-                // System.out.println("Option à venir .....");
-                // s = new Multimedia(); //choix 4 crée un Magazine
-                // break;
-                // case 5:
-                // System.out.println("Option à venir .....");
-                // s = new Multimedia(); //choix 5 crée Carte Michelin
-                // break;
-                default:
-                    try {
+		if (supports == null) {
+			supports = new ArrayList<Support>();
+		}
 
-                        SaveNLoad snl = new SaveNLoad();
-                        snl.save(supports);
-                    } catch (Exception e) {
-                        System.out.println("Une erreur est survenue");
-                    }
-                    break;
+		// Question pour le choix de l'objet a créer
+		String question = "Que voulez créer ?\n1- Livre\n2- DVD\n3- CD\n8- Display\n9- Save\n0- STOP";
+		Integer userChoice = Scan.scanInteger(question);
+		Support s = null;
+		// tant que la réponse n'est pas 0 (STOP) on continue la création d'objet
+		while (userChoice != 0) {
 
-            }
-            if (s==null){
-            System.out.println("Oh no s is null");
-            }
+			switch (userChoice) {
+			case 1:
+				s = new Livre(); // choix 1 crée un livre
+				break;
+			case 2:
+				s = new Dvd(); // choix 2 crée un Dvd
+				break;
+			case 3:
+				s = new Cd(); // choix 3 crée un Cd
+				break;
+			// case 4:
+			// System.out.println("Option à venir .....");
+			// s = new Multimedia(); //choix 4 crée un Magazine
+			// break;
+			// case 5:
+			// System.out.println("Option à venir .....");
+			// s = new Multimedia(); //choix 5 crée Carte Michelin
+			// break;
+			case 8:
+				for (Support support : supports) {
+					System.out.println(support);
+				}
+			case 9:
+				try {
+					SaveNLoad snl = new SaveNLoad();
+					snl.save(supports);
+				} catch (Exception e) {
+					System.out.println("Une erreur est survenue");
+				}
+			}
 
-            supports.add(s);
-            System.out.println(s); // print l'object créé
+			if (s != null) {
+				supports.add(s);
+				System.out.println(s); // print l'object créé
+			}
+			userChoice = Scan.scanInteger(question);
 
-            userChoice = Scan.scanInteger(question);
+		}
 
-        }
-
-    }
+	}
 }
--- src/Utils/SaveNLoad.java
+++ src/Utils/SaveNLoad.java
@@ -5,30 +5,41 @@ import java.beans.XMLEncoder;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 import Format.Support;
 
 public class SaveNLoad {
-   
-    public void save(ArrayList<Support> supports) throws IOException {
-        
-        FileOutputStream fos = new FileOutputStream("bdd.xml");
-        XMLEncoder encoder = new XMLEncoder(fos);
-        encoder.writeObject(supports);
-        encoder.close();
-        fos.close();
-        System.out.println("Saved !");
-    }
 
-    public ArrayList<Support> load() throws IOException {
-        FileInputStream fis = new FileInputStream("bdd.xml");
-        XMLDecoder decoder = new XMLDecoder(fis);
-        ArrayList<Support> supports = (ArrayList<Support>) decoder.readObject();
-        decoder.close();
-        fis.close();
+	public void save(ArrayList<Support> supports) throws IOException {
+		try (FileOutputStream fileOut = new FileOutputStream("bdd.ser");
+				ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+			out.writeObject(supports);
+		}
 
-return supports;
-    }
+//        FileOutputStream fos = new FileOutputStream("bdd.xml");
+//        XMLEncoder encoder = new XMLEncoder(fos);
+//        encoder.writeObject(supports);
+//        encoder.close();
+//        fos.close();
+		System.out.println("Saved !");
+	}
+
+	public ArrayList<Support> load() throws IOException, ClassNotFoundException {
+		try (FileInputStream fileIn = new FileInputStream("bdd.ser");
+				ObjectInputStream in = new ObjectInputStream(fileIn)) {
+			return (ArrayList<Support>) in.readObject();
+		}
+
+//        FileInputStream fis = new FileInputStream("bdd.xml");
+//        XMLDecoder decoder = new XMLDecoder(fis);
+//        ArrayList<Support> supports = (ArrayList<Support>) decoder.readObject();
+//        decoder.close();
+//        fis.close();
+
+//return supports;
+	}
 
 }
